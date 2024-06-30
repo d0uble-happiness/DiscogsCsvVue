1a55a89a5751582741ae570c6178e1a6f7a61101
d0uble-happiness <double.happiness.records@gmail.com>
2024-06-30 11:13:36 +0100

    Created parser, updated App.vue, deleted csv.ts

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,39 @@
 <template>
-  <div>
-    <FileUpload @file="setFile" />
-    <ParseCsvToArray v-if="file" :file="file" />
-    <FetchRelease v-if="data" :data="data" />
-    <ProcessReleaseData v-if="formattedData" :data="formattedData" />
-    <DownloadCSV v-if="csvContent" :data="csvContent" />
-  </div>
+    <div>
+        <FileUpload @file="setFile" />
+    </div>
 
-  <div>
-    <p v-for="row of formattedData" v-bind:key="row.id">
-      {{ row }}
-    </p>
-  </div>
+    <div>
+        <p v-for="row of data" :key="row">
+            {{ row }}
+        </p>
+    </div>
 </template>
 
 <script lang="ts">
-import DownloadCSV from './components/DownloadCSV';
-import FetchRelease from './components/FetchRelease';
 import { defineComponent } from 'vue';
-import FileUpload from './components/FileUpload.vue';
-import ParseCsvToArray from './components/ParseCsvToArray.vue';
-import ProcessReleaseData from './components/ProcessReleaseData';
+import FileUpload from '@/components/FileUpload.vue';
+import { fetchRelease, parseCsvToArray } from "@/parser";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    FileUpload,
-    ParseCsvToArray,
-    FetchRelease,
-    ProcessReleaseData,
-    DownloadCSV
-  },
-  data() {
-    return {
-      file: null as null | File,
-    }
-  },
-  methods: {
-    setFile(file: File) {
-      console.log("Received file:", file)
-      this.file = file;
-    }
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  props: ['data', 'formattedData', 'csvContent']
+    name: 'App',
+    components: {
+        FileUpload,
+    },
+    data() {
+        return {
+            data: null as null | string[],
+        }
+    },
+    methods: {
+        async setFile(file: File) {
+            this.data = await parseCsvToArray(file)
+        }
+    },
+    watch: {
+        data(data) {
+            fetchRelease(data)
+        }
+    },
 });
 </script>
-
-<style></style>
